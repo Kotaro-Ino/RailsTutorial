58621058c7672552250ca41d898d1fdfe8f545c1
Kotaro.Ino <kotaro.ino@pa-rk.co.jp>
2023-05-22 13:40:25 +0900

    [add]11章完了

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,11 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = 'localhost:3000'  # ここをコピペすると失敗します。自分の環境に合わせてください。
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+
   # Do not eager load code on boot.
   config.eager_load = false
 
@@ -51,4 +56,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  
 end
